5713c93c97d5e645f5cf318e4694250379685d0a
ttwings <ttwings@qq.com>
2019-01-08 23:58:51 +0800

    表情动画测试

--- characters/Player.gd
+++ characters/Player.gd
@@ -1,5 +1,10 @@
 extends "res://characters/Character.gd"
 
+
+func _ready():
+	# $Balloon.hide()
+	pass
+	
 func _process(delta):
 	if can_move:
 		for dir in moves.keys():
@@ -7,8 +12,10 @@ func _process(delta):
 				facing = dir
 				move(facing)
 	
-	if Input.is_action_pressed("battle_attack"):
+	if Input.is_action_pressed("A"):
 		#attack(facing)
-		print("attack")
+		#$"Balloon/AnimationPlayer".play("ballon05")
+		$"Skill/AnimatedSprite".play('default')
+		# $Balloon.hide()
 
 	
