8a94ae4c9c0ccc2100c307f5167e554f1037cd4c
ttwings <ttwings@qq.com>
2019-01-15 01:11:05 +0800

    Animation test
    战斗动画测试，godot动画方面很强大。

--- characters/Player.gd
+++ characters/Player.gd
@@ -1,6 +1,6 @@
 extends "res://characters/Character.gd"
 
-
+var skill_anim = null
 func _ready():
 	# $Balloon.hide()
 	pass
@@ -10,12 +10,28 @@ func _process(delta):
 		for dir in moves.keys():
 			if Input.is_action_pressed(dir):
 				facing = dir
+				if dir == 'left':
+					$Balloon.flip_h = true
+					$Balloon.offset = Vector2(-16,0)
+				elif dir == 'right':
+					$Balloon.flip_h = false
+					$Balloon.offset = Vector2(16,0)
+				else:
+					$Balloon.flip_h = false
+					$Balloon.offset = Vector2(0,0)
 				move(facing)
 	
 	if Input.is_action_pressed("A"):
 		#attack(facing)
 		$"Balloon/AnimationPlayer".play("ballon05")
-		$"Skill/AnimatedSprite".play('default')
+		#$"Skill/AnimatedSprite".play('default')
 		# $Balloon.hide()
-
+	if Input.is_action_just_pressed("B"):
+		skill_anim = $Skill/Area2D/AnimatedSprite
+		#skill_anim.show()
+		skill_anim.position = moves[facing]*48
+		skill_anim.play('default')
+		skill_anim.frame = 0
+		$Skill/Label/AnimationPlayer.play("anim_skill")
+		#skill_anim.hide()
 	
